bd68563ccef6a0822386ce45798b8c999522c42b
Mustaqhasan <shaikhmustaq7714@gmail.com>
2023-02-12 01:40:09 +0530

    Added files

---
+++ navbar/script/navbar.js
@@ -0,0 +1,11 @@
+let search = ()=>{
+    let input = document.getElementById("search").value;
+    console.log(input)
+}
+
+let startFree =()=>{
+    window.location.href="";
+}
+
+
+// for product section
